6eed8279d4fb35e1fbfc26c2f5f14ee58b5a632e
notfound48 <just_bis@mail.ru>
2016-01-15 14:55:28 +0300

    Исправлена логическая ошибка

--- functions.sh
+++ functions.sh
@@ -20,75 +20,23 @@ tarBackup(){
 		# Перемещаемся в целевой каталог
 		cd ${filesTargetDir}
 
-		tar $filesTarCreateParam --file=${filesBackupsDir}/$1.tar.gz \
-		--listed-incremental=${scriptDir}/meta/$1 --exclude-from=${filesExclude} ./
+		tar $filesTarCreateParam --file=${filesBackupsDir}/archives/$1.tar.gz \
+		--listed-incremental=${filesBackupsDir}/meta/$1 --exclude-from=${filesExclude} ./
 
 		# Вернулись в каталог скрипта
 		cd ${scriptDir}
 
 		# Cинхронизация с amazon
-		if [ "$filesUseAws" == "yes" ];
+		#if [ "$filesUseAws" == "yes" ];
 
-			then
+			#then
 
-				loging "Syncing with AWS"
+				#loging "Syncing with AWS"
 
-				s3cmd put ${filesBackupsDir}/$1.tar.gz s3://${awsBucketName}/files/
+				#s3cmd put ${filesBackupsDir}/$1.tar.gz s3://${awsBucketName}/files/
 
-		fi
+		#fi
 	
-}
-
-tarBackupMonth(){
-
-		loging "Making full month backup"
-
-		rm -f ${scriptDir}/meta/month
-
-		tarBackup month
-
-}
-
-tarBackupWeek(){
-
-		if [ ! -f ${scriptDir}/meta/month ];
-			
-			then
-
-				loging "Have not month backup"
-
-				tarBackupMonth
-
-		fi
-
-		loging "Making incremental week backup"
-
-		cp ${scriptDir}/meta/month ${scriptDir}/meta/week
-
-		tarBackup week
-
-}
-
-tarBackupDay(){
-
-		if [ ! -f ${scriptDir}/meta/week ];
-			
-			then
-
-				loging "Have not week backup"
-
-				tarBackupWeek
-
-		fi
-
-		loging "Making incremental day backup"
-
-		execDay=`date +%A`
-
-		cp ${scriptDir}/meta/week ${scriptDir}/meta/${execDay,,}
-
-		tarBackup ${execDay,,}
-
 }
 
 # Бэкап MySQL
@@ -96,20 +44,9 @@ mysqlBackup(){
 
 	loging "Making MySQL backup"
 
-	[ `date +%d` = $filesMonthDay ] && mysqlDumpName="month" || [ `date +%u` = 1 ] && mysqlDumpName="week" || mysqlDumpName=`date +%A`
+	mkdir -p ${mysqlBackupsDir}/${nowMonth}
 
-	mysqldump -h ${mysqlHost} -P ${mysqlPort} -u ${mysqlUser} -p${mysqlPassword} ${mysqlDatabases} | gzip > ${mysqlBackupsDir}/${mysqlDumpName,,}.sql.gz
-
-	# Cинхронизация с amazon
-	if [ "$mysqlUseAws" == "yes" ];
-
-		then
-
-			loging "Syncing with AWS"
-
-			s3cmd put ${mysqlBackupsDir}/${mysqlDumpName,,}.sql.gz s3://${awsBucketName}/mysql/
-
-	fi
+	mysqldump -h ${mysqlHost} -P ${mysqlPort} -u ${mysqlUser} -p${mysqlPassword} ${mysqlDatabases} | gzip > ${mysqlBackupsDir}/${nowMonth}/${nowDay}.sql.gz
 
 }
 
--- make.sh
+++ make.sh
@@ -13,13 +13,62 @@ loging "Start working"
 
 loging "Start backup files"
 
-# Месячный бекап
-[ `date +%d` = $filesMonthDay ] && tarBackupMonth
+nowMonth=`date +%m`
+nowDay=`date +%d`
 
-# Недельный инкрементный бекап
-[ `date +%u` = 1 ] && tarBackupWeek || tarBackupDay
+if [ ! -f ${filesBackupsDir}/meta/${nowMonth}/full ];
+	then
+
+	loging "Making full month backup ${nowMonth}"
+
+	mkdir -p ${filesBackupsDir}/meta/${nowMonth}
+	mkdir -p ${filesBackupsDir}/archives/${nowMonth}
+
+	rm -f ${filesBackupsDir}/meta/${nowMonth}/full
+
+	tarBackup ${nowMonth}/full
+
+fi
+
+loging "Making incremental regular backup ${nowMonth}/${nowDay}"
+
+cp ${filesBackupsDir}/meta/${nowMonth}/full ${filesBackupsDir}/meta/${nowMonth}/${nowDay}
+
+tarBackup ${nowMonth}/${nowDay}
+
+loging "Clearing old files backups"
+
+find ${filesBackupsDir}/archives -type d -mtime +$[$filesMonthsCount*31] | xargs rm -rf
+find ${filesBackupsDir}/meta -type d -mtime +$[$filesMonthsCount*31] | xargs rm -rf
+
+if [ "$filesUseAws" == "yes" ];
+	then
+
+	loging "Syncing with AWS"
+
+	s3cmd --acl-private --bucket-location=EU --guess-mime-type sync ${filesBackupsDir}/ s3://${awsBucketName}/files/
+
+fi
 
 # MySQL бекап
-[ "$mysqlMakeBackups" == "yes" ] && mysqlBackup
+if [ "$mysqlMakeBackups" == "yes" ];
+	then
+
+	mysqlBackup 
+
+	loging "Clearing old MySQL backups"
+
+	find ${mysqlBackupsDir} -type d -mtime +$[$filesMonthsCount*31] | xargs rm -rf
+
+	if [ "$mysqlUseAws" == "yes" ];
+	then
+
+	loging "Syncing with AWS"
+
+	s3cmd --acl-private --bucket-location=EU --guess-mime-type sync ${mysqlBackupsDir}/ s3://${awsBucketName}/mysql/
+
+	fi
+
+fi
 
 loging "Finish working"
